2a383790dc64ab28c39e515b2d1a43e3b864dd5a
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-01-29 05:19:39 -0800

    edited function

--- 00-filter/app.js
+++ 00-filter/app.js
@@ -1,24 +1,24 @@
 //get result element
-const result = document.querySelect('#result');
+const resultCont = document.querySelector('#result');
 //get input filter element
-const input = document.querySelect('#filter');
+const filter = document.querySelector('#filter');
 //define an array variable
 const listItems = [];
 
 //add an event listener to filter element
 filter.addEventListener('input', function(e){
     // filterData(e.???.???);
-    filterData(e.target.value);
+  filterData(e.target.value);
 })
 
 getData();
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
-    const { results } = await res.json()
+    const res = await fetch('https://randomuser.me/api?results=50');
+    const { results } = await res.json();
 
     // Clear result
-    result.innerHTML = ''
+    result.innerHTML = '';
 
 
    // name
@@ -30,33 +30,52 @@ async function getData() {
         //p -- any info you wanna put
     // </div
 
-    for (const element of results) {
+        // img.src = `${element.picture.large}`;
+        // div.classList.add('user-info');
+        // h4 = `${element.id.name}`
+
+
+    results.forEach((re) => {
         const li = document.createElement('li');
+        // result.appendedChild(li);
+
+
         const img = document.createElement('img');
-        const div = document.createElement('div');
-        const h4 = document.createElement('h4');
-        const p = document.createElement('p');
-
-        result.appendedChild(li);
+        img.src = `${re.picture.thumbnail}`; 
         li.appendChild(img);
-        li.appendChild(div);
-        div.appendChild(h4);
-        div.appendChild(p);
 
-        
+        const container = document.createElement("div");
+        li.appendChild(container);
+        container.classList.add("user-info");
 
-    }
+        const name = document.createElement("h4");
+        container.innerText = `${re.name.first} ${re.name.last}`;
+        container.appendChild(name);
+
+        const info = document.createElement("p");
+        info.innerText = `${re.location.city}, ${re.location.country}`;
+        container.appendChild(info);
+
+
+        listItems.push(li);
+
+
+        resultCont.appendChild(li);
+
+    });
 }
 
 function filterData(searchTerm) {
     listItems.forEach(item => {
         /* add conditional logic below */
-        if(listItems.includes(searchTerm)) {
+        const search = item.innerText.toLowerCase();
+
+        if(search.includes(searchTerm.toLowerCase())) {
             //remove the class of .hide
-            item.classList.remove('hide');
+            item.classList.remove("hide");
         } else {
             //add the class of .hide
-            item.classList.add('hide');
+            item.classList.add("hide");
         }
-    })
+    });
 }
